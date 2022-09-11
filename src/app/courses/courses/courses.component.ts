dd3c0f99d3f6baaee0ee354356600d26b1121007
Bruna Nicolau <brunanicolauolivera@gmail.com>
2022-09-11 20:28:32 -0300

    basico para bater no back

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 import { CousesList } from '../model/couses-list';
 import { CoursesService } from '../service/service.service';
 
@@ -9,16 +12,42 @@ import { CoursesService } from '../service/service.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: CousesList[] = [];
+  courses$: Observable<CousesList[]>;
   displayedColumns = ['name', 'category'];
 
-
   constructor(
-    private service: CoursesService
-  ) { }
+    private service: CoursesService,
+    public dialog: MatDialog,
+  ) {
+    // this.refresh();
+    this.courses$ = this.service.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos.');
+          return of([])
+        })
+      );
+  }
+  
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+  
+  // refresh() {
+  // this.courses$ = this.service.list()
+  //   .pipe(
+  //     catchError(error => {
+  //       this.onError('Erro ao carregar cursos.');
+  //       return of([])
+  //     })
+  //   );
+  // }
 
   ngOnInit(): void {
-    this.courses = this.service.list()
   }
+
+
 
 }
--- src/app/courses/service/service.service.ts
+++ src/app/courses/service/service.service.ts
@@ -2,19 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { CousesList } from '../model/couses-list';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '../../assets/courses.json';
+
   constructor(
     private httpClient: HttpClient
   ) { }
 
-  list(): CousesList[] {
-    return [
-      { _id: '123', name: 'angular', category: 'front' }
-    ];
+  list() {
+    return this.httpClient.get<CousesList[]>(this.API)
+      .pipe(
+        first(),
+        delay(900),
+        tap(courses => console.log(courses))
+      );
   }
 }
